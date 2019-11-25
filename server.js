f26022b707696d7bfa8b1f7997a14ad141059836
Pankaj Tanwar Banna <2016ucp1381@mnit.ac.in>
2019-11-25 15:41:39 +0530

    One more commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express  = require('express');
 var app = express();
 var morgan = require('morgan');             // middleware to log http requests
-var port = 3500 || process.env.PORT;
+var port = process.env.PORT || 3500;
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var router = express.Router();
